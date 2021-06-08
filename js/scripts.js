0e9d7679122a3069378c674db30865752dd286ba
Vaughn Moriniere <vaughnmoriniere@yahoo.com>
2021-06-07 21:24:19 -0500

    Managed to get the link to work to extrenal site but..
    Need to fix the visual when the site comes up, it looks strange right now.

--- js/scripts.js
+++ js/scripts.js
@@ -53,20 +53,29 @@
     // Collapse the navbar when page is scrolled
     $(window).scroll(navbarCollapse);
 
+
+
+
+
+// THIS BELOW WAS ORIGINALLY WRITTEN TO MAGINFY IMAGES THAT THE THUMBNAILS ARE, 
+// HOWEVER NOW THEY JUST LINK TO A SEPERATE SITE, 
+// THEREFORE I CAN EASILY INSERT MY APPS INTO THIS GALLERY
+
+
     // Magnific popup calls
-    $('#portfolio').magnificPopup({
-        delegate: 'a',
-        type: 'image',
-        tLoading: 'Loading image #%curr%...',
-        mainClass: 'mfp-img-mobile',
-        gallery: {
-            enabled: true,
-            navigateByImgClick: true,
-            preload: [0, 1]
-        },
-        image: {
-            tError: '<a href="%url%">The image #%curr%</a> could not be loaded.'
-        }
-    });
+    // $('#portfolio').magnificPopup({
+    //     delegate: 'a',
+    //     type: 'image',
+    //     tLoading: 'Loading image #%curr%...',
+    //     mainClass: 'mfp-img-mobile',
+    //     gallery: {
+    //         enabled: true,
+    //         navigateByImgClick: true,
+    //         preload: [0, 1]
+    //     },
+    //     image: {
+    //         tError: '<a href="%url%">The image #%curr%</a> could not be loaded.'
+    //     }
+    // });
 
 })(jQuery); // End of use strict
